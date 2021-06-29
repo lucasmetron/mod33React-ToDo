2aa6e2d83f01acab345f9f0d0d4ca5c8556332b2
lucasmetron <lucasmetron@gmail.com>
2021-06-29 15:59:19 -0300

    como capturar input com react

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import { useState } from 'react';
+import './Todo.css'
 
 
 function Todo() {
 
+    const [text, setText] = useState("");
+
+    function handleChange(event) {
+        console.log(event)
+        let t = event.target.value;
+        setText(t)
+
+    }
+
     return (
-        <h1>Teste</h1>
+        <div className="container">
+            <h1>Teste</h1>
+            <form>
+                <input onChange={handleChange} type="text" />
+                <button>Add</button>
+            </form>
+
+            <ul>
+                <li>{text}</li>
+            </ul>
+        </div>
+
     )
 
 }
+
+
+
+
 
 
 export default Todo;
