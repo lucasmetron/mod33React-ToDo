ed590c71db6a8ef6082fddf68a88ebdab10ba1c5
lucasmetron <lucasmetron@gmail.com>
2021-06-30 14:02:03 -0300

    add hooks

--- src/Todo.js
+++ src/Todo.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Todo.css'
 import List from './components/List';
 import TodoForm from './components/TodoForm';
 import Item from './components/Item';
 
+const SAVED_ITEMS = 'savedItems'
 
 function Todo() {
 
     const [items, setItems] = useState([]);
 
+    useEffect(() => {
+        let savedItems = JSON.parse(localStorage.getItem(SAVED_ITEMS));
+        if (savedItems) {
+            setItems(savedItems);
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem(SAVED_ITEMS, JSON.stringify(items))
+    }, [items])
+
     function onAddItem(text) {
 
         let it = new Item(text)
         setItems([...items, it])
+
 
     }
 
