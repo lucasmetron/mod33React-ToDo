01c3790e2c58e797cf71ac2a54d27e2107019341
lucasmetron <lucasmetron@gmail.com>
2021-06-30 10:09:00 -0300

    função deletar

--- src/List.js
+++ src/List.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 function List(props) {
+
     return (
         <ul >
             {props.items.map(item => {
                 return (
-                    <li key={item.id}>{item.text}</li>
+                    <li key={item.id}>
+                        {item.text}
+                        <button onClick={() => { props.onItemDeleted(item) }}>Deletar</button>
+                    </li>
                 )
             })}
         </ul>
--- src/Todo.js
+++ src/Todo.js
@@ -11,15 +11,26 @@ function Todo() {
     function onAddItem(text) {
 
         let it = new Item(text)
-
         setItems([...items, it])
+
+    }
+
+    function onItemDeleted(item) {
+        console.log(item)
+        let filteredItems = items.filter(it => {
+            console.log(it)
+            return (
+                it.id !== item.id
+            )
+        })
+        setItems(filteredItems)
     }
 
     return (
         <div className="container">
             <h1>To Do</h1>
             <TodoForm onAddItem={onAddItem}></TodoForm>
-            <List items={items}></List>
+            <List onItemDeleted={onItemDeleted} items={items}></List>
         </div>)
 
 }
